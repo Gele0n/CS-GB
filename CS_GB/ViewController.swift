a294250ce51dcc560bc7b1952d41cf7e8d15c38e
Gele0n <Gele0n@mail.ru>
2021-07-17 21:15:17 +0700

    Firs Push

---
+++ CS_GB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS_GB
+//
+//  Created by Ярослав Троянов on 17.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
